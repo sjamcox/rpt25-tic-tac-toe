7dc2de8779bf52a59782e3e812ebd418335551cd
Sam Cox <sjamcox@gmail.com>
2020-11-15 12:47:20 -0800

    Add Board class and render to app

--- app.js
+++ app.js
@@ -1,2 +1,32 @@
 const app = document.getElementById('app')
-app.innerText = 'Connected!';
+
+class Board {
+
+  constructor() {
+    this.grid = [
+      ['X','X','X'],
+      ['X','X','X'],
+      ['X','X','X']
+    ];
+  }
+
+  render() {
+    app.innerHTML = '';
+    let board = document.createElement('div');
+    board.setAttribute('id', 'board');
+    this.grid.forEach((r, i) => {
+      let row = document.createElement('div');
+      row.setAttribute('class', 'row')
+      r.forEach(cell => {
+        let box = document.createElement('div');
+        box.innerHTML = cell;
+        row.appendChild(box);
+      })
+      board.appendChild(row);
+    })
+    app.appendChild(board);
+  }
+}
+
+let game = new Board();
+game.render();
