559cb1d669a882e0fec0d0f35a5dc5c3ba98ed8c
Sam Cox <sjamcox@gmail.com>
2020-11-15 14:23:45 -0800

    Change handleClick to update this.grid, add methods to check for win condition

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ class Board {
       ['','',''],
       ['','','']
     ];
-    this.next = 'X'
+    this.next = 'X';
+    this.winner = null;
   }
 
   updateNext() {
@@ -16,8 +17,33 @@ class Board {
   }
 
   handleClick(e) {
-    e.target.innerText = this.next;
-    this.updateNext();
+    let row = e.target.getAttribute('data-row');
+    let col = e.target.getAttribute('data-col');
+    if (this.grid[row][col] === '' && this.winner === null) {
+      this.grid[row][col] = this.next;
+      this.updateNext();
+      this.checkWinConditions();
+      this.render();
+    }
+  }
+
+  checkHorizontalWin() {
+    this.grid.forEach(row => {
+      if (row[0] === row[1] && row[0] === row[2]) {
+        if (row[0] === 'X') {
+          this.winner = 'X';
+        } else if (row[0] === 'O') {
+          this.winner = 'O';
+        }
+      }
+    });
+  }
+
+  checkWinConditions() {
+    this.checkHorizontalWin();
+    if (this.winner) {
+      console.log(`${this.winner} Wins!`)
+    }
   }
 
   render() {
@@ -28,10 +54,12 @@ class Board {
       let row = document.createElement('div');
       row.setAttribute('class', 'row')
       row.setAttribute('style', 'display: flex;')
-      r.forEach(element => {
+      r.forEach((element, j) => {
         let box = document.createElement('div');
         box.setAttribute('class', 'box')
         box.setAttribute('style', 'width: 15px; height: 15px; padding: 50px; border: 1px solid #cccccc;')
+        box.setAttribute('data-row', `${i}`)
+        box.setAttribute('data-col', `${j}`)
         box.innerHTML = element;
           box.addEventListener('click', (e) => {
             this.handleClick(e);
